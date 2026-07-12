a600f3a0334504d27f55f196c55be351a0c449be
Mohit Parmar <parmarmohit0627@gmail.com>
2026-07-12 10:56:01 +0530

    feat(backend): setup express server and application foundation

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,21 @@
-// App entry point
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const routes = require('./routes');
+const errorHandler = require('./middleware/error.middleware');
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', routes);
+
+app.use(errorHandler);
+
+module.exports = app;
